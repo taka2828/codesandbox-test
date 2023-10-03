07acb9d491dd5107ebe08f0b08c3e99397d7f3b1
taka <67226945+taka2828@users.noreply.github.com>
2023-10-03 07:10:35 +0000

    変数メモ

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,52 @@
-import "./styles.css";
+/**
+ * const, let
+ */
+var val1 = "var変数";
+console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var変数は上書き可能
+val1 = "var上書き";
+console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "var再宣言";
+console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書き可能
+val2 = "let上書き";
+console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let再宣言";
+
+// // const
+// const val3 = "const変数";
+// console.log(val3);
+
+// // constは上書き、再宣言不可能
+// val3 = "const上書き";
+// const val3 = "const再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+const val4 = {
+  name: "hoge",
+  age: 10,
+};
+console.log(val4);
+
+val4.name = "fuga";
+console.log(val4);
+
+val4.address = "aaabbb";
+console.log(val4);
+
+// 配列も可能
+const val5 = ["dog", "cat"];
+console.log(val5);
+
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
